84b8320d5cb0a731597edf3557f4d90f327fbf79
Itsjustme27 <reachprayush@gmail.com>
2026-02-23 17:10:25 +0545

    need fixes

--- files/src/context/AuthContext.js
+++ files/src/context/AuthContext.js
@@ -1,10 +1,15 @@
 import React, { createContext, useState, useEffect } from 'react';
+import siemApi from './api/siemApi';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [token, setToken] = useState(localStorage.getItem('siem_token'));
+    
+    useEffect(() => {
+      const interceptor = siemApi 
+    });
 
     useEffect(() => {
         if (token) {
